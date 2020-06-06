6cd9fd7cc75d1dbc6f948a3d243116152a648929
Pete Bachant <petebachant@gmail.com>
2020-06-06 15:04:23 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Do black lives matter?
+
+A Python script to help answer this question.
+
+To compute the answer, simply run
+```
+python do_black_lives_matter.py
+```
---
+++ do_black_lives_matter.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+"""Do black lives matter?"""
+
+
+class Life:
+    matters = True
+
+
+class Person:
+    def __init__(self, skin_color):
+        self.skin_color = skin_color
+        self.life = Life()
+
+
+if __name__ == "__main__":
+    people = []
+    black_people = []
+    for n in range(100):
+        if n % 2 == 0:
+            p = Person(skin_color="black")
+            black_people.append(p)
+        else:
+            p = Person(skin_color="white")
+        people.append(p)
+
+    all_lives_matter = all([person.life.matters for person in people])
+    black_lives_matter = all([person.life.matters for person in black_people])
+
+    # No real reason to assert this because it's not really what we're asking,
+    # but we'll do it anyway (silently)
+    assert all_lives_matter
+
+    # Check some logic here just in case
+    if black_lives_matter and not all_lives_matter:
+        raise SystemError
+
+    # Okay, if we've made it here, it's a good sign
+    # Now let's answer the question at hand
+    if black_lives_matter:
+        print("Yes")
+    else:
+        print("No")
